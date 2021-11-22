5e49913a691f9eb0822d2446edc5191cba63305f
emoralVisionary <emoralvisionary@gmail.com>
2021-11-22 16:16:12 +0900

    Style: Add styling to  movie detail.vuegenre, vote_average, popularity printing introduce navtabs for overview, reviews, trailer

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -5,55 +5,95 @@
         <img :src="posterPath" class="img-fluid" alt="">
       </div>
       <div class="col-8">
-        <h2 class="text-start">{{ movieData.title }}</h2>
-        <p class="text-start">{{ movieData.overview }}</p>
-        <p>{{ movieData }}</p>
+        <div>
+          <span class="fs-2">{{ movieData.title }}</span>
+          <small class="ms-3">({{ movieData.original_title }}, {{ movieData.release_date }})</small>
+        </div>
+        <hr>
+        <div class="d-flex justify-content-around">
+          <span>
+            <strong>장르</strong>
+            {{ genres }}
+          </span>
+          <span>
+            <strong>평점</strong>
+            {{ movieData.vote_average }}
+          </span>
+          <span>
+            <strong>누적관객</strong>
+            {{ movieData.popularity }}
+          </span>
+        </div>
+        <hr>
+        <ul class="nav nav-tabs" id="Tab" role="tablist">
+          <li class="nav-item" role="presentation">
+            <button class="nav-link active" id="overview-tab" data-bs-toggle="tab" data-bs-target="#overview" type="button" role="tab" aria-controls="overview" aria-selected="true">줄거리</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="reviews-tab" data-bs-toggle="tab" data-bs-target="#reviews" type="button" role="tab" aria-controls="reviews" aria-selected="false">리뷰</button>
+          </li>
+          <li class="nav-item" role="presentation">
+            <button class="nav-link" id="trailer-tab" data-bs-toggle="tab" data-bs-target="#trailer" type="button" role="tab" aria-controls="trailer" aria-selected="false">트래일러</button>
+          </li>
+        </ul>
+        <div class="tab-content" id="myTabContent">
+          <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview-tab">
+            <p class="text-start">{{ movieData.overview }}</p>
+          </div>
+          <div class="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="reviews-tab">
+            <div>
+              <div class="row justify-content-between my-2">
+                <div class="col-3 fs-4"><strong>리뷰 목록</strong></div>
+                <div class="col-2">
+                  <button
+                    class="btn btn-light btn-sm"
+                    @click="getReviewForm"
+                  >
+                    리뷰 작성
+                  </button>        
+                </div>
+              </div>
+              <table class="table table-dark table-hover">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">작성자</th>
+                    <th scope="col">제목</th>
+                    <th scope="col">평점</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr
+                    v-for="review in reviews" 
+                    :key="review.id" 
+                  >
+                    <th scope="row">{{ review.id }}</th>
+                    <td>{{ review.userName }}</td>
+                    <td>
+                      <router-link 
+                        class="text-reset"
+                        :to="{ 
+                          name: 'ReviewDetail', 
+                          params: { movieID: movieData.id, reviewId: review.id, review: review },        
+                          }"
+                        >
+                        {{ review.title }}
+                      </router-link>
+                    </td>
+                    <td>{{ review.rank }}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="tab-pane fade" id="trailer" role="tabpanel" aria-labelledby="trailer-tab">
+            ...
+          </div>
+        </div>
       </div> 
     </div>
-    <div>
-      <div class="row justify-content-between my-2">
-        <div class="col-2 fs-3">리뷰 목록</div>
-        <div class="col-2">
-          <button
-            class="btn btn-light btn-sm"
-            @click="getReviewForm"
-          >
-            리뷰 작성
-          </button>        
-        </div>
-      </div>
-      <table class="table table-dark table-hover">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">작성자</th>
-            <th scope="col">제목</th>
-            <th scope="col">평점</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr
-            v-for="review in reviews" 
-            :key="review.id" 
-          >
-            <th scope="row">{{ review.id }}</th>
-            <td>{{ review.userName }}</td>
-            <td>
-              <router-link 
-                class="text-reset"
-                :to="{ 
-                  name: 'ReviewDetail', 
-                  params: { movieID: movieData.id, reviewId: review.id, review: review },        
-                  }"
-                >
-                {{ review.title }}
-              </router-link>
-            </td>
-            <td>{{ review.rank }}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <hr>
+    <p>{{ movieData }}</p>
   </div>
 </template>
 
@@ -62,6 +102,31 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'MovieDetail',
+  data: function() {
+    return {
+      genreRef: {
+        28: "액션",
+        12: "모험",
+        16: "애니메이션",
+        35: "코미디",
+        80: "범죄",
+        99: "다큐멘터리",
+        18: "드라마",
+        10751: "가족",
+        14: "판타지",
+        36: "역사",
+        27: "공포",
+        10402: "음악",
+        9648: "미스터리",
+        10749: "로맨스",
+        878: "SF",
+        10770: "TV 영화",
+        53: "스릴러",
+        10752: "전쟁",
+        37: "서부"
+      }
+    }
+  },
   methods: {
     getReviewForm() {
       this.$router.push({ name: 'MovieReview', params: { movieId: this.movieData.id} })
@@ -75,6 +140,18 @@ export default {
     posterPath() {
       return `https://image.tmdb.org/t/p/w500/${this.movieData.poster_path}`
     },
+    genres() {
+      let genreStr = ''
+      this.movieData.genre_ids.forEach(id => {
+        if (!genreStr) {
+          genreStr = this.genreRef[`${id}`]
+        } else {
+          genreStr = genreStr + ', ' + this.genreRef[`${id}`]
+        }
+      })
+
+      return genreStr
+    }
   },
 }
 </script>
@@ -83,4 +160,7 @@ export default {
  /* .table {
    color: antiquewhite;
  } */
+  .nav-link {
+    color: wheat;
+  }
 </style>
