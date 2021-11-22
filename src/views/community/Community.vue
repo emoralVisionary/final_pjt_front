b5908c2af01e18a2f6c60d12d3b87ca507764739
emoralVisionary <emoralvisionary@gmail.com>
2021-11-22 22:25:31 +0900

    style: Add Community post list styling ...ing

--- src/views/community/Community.vue
+++ src/views/community/Community.vue
@@ -1,24 +1,59 @@
 <template>
   <div>
-    <button @click="getPostForm">글쓰기</button>
-    <p v-for="post in this.posts" :key=post.id>
-      <router-link :to="{ 
-        name: 'PostDetail', 
-        params: { postId: post.id },
-        }">
-        {{ post.id }} | {{ post.title }} 
-      </router-link>
-    </p>
+    <div class="row justify-content-between my-2">
+      <div class="col-3 fs-2"><strong>자유 게시판</strong></div>
+      <div class="col-2">
+        <button
+          class="btn btn-primary btn-sm"
+          @click="getPostForm"
+        >
+          글쓰기
+        </button>        
+      </div>
+    </div>
+    <table class="table table-dark table-hover">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">작성자</th>
+          <th scope="col">제목</th>
+          <th scope="col">게시일</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="post in posts" 
+          :key="post.id" 
+        >
+          <th scope="row">{{ post.id }}</th>
+          <td>{{ post.userName }}</td>
+          <td>
+            <router-link 
+              :to="{ 
+                name: 'PostDetail', 
+                params: { postId: post.id },
+              }"
+              class="text-reset"
+            >
+              {{ post.title }} 
+            </router-link>
+          </td>
+          <td>{{ postDate[`${post.id}`] }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import moment from 'moment'
 
 export default {
   name: 'Community',
   data: function () {
     return {
+      postDate: {},
     }
   },
   methods: {
@@ -35,6 +70,18 @@ export default {
   },
   computed: {
     ...mapState(['posts', 'token']),
+  },
+  watch: {
+    getPostDate() {
+      this.posts.forEach(element => {
+        if (!Object.keys(this.postDate).includes(element.id)) {
+          Object.defineProperty(this.postDate, element.id, { 
+            value: moment(element.created_at).format('YYYY-MM-DD, hh:mm a'), 
+            writable: false 
+          })
+        }
+      });
+    }
   }
 }
 </script>
